08cbf822dc1486f83ce14a20b3aa3e007ca12192
Luciano Ramello <luchosr@gmail.com>
2025-03-05 18:00:33 +0100

    update cors config

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,15 +1,33 @@
+// import { CorsOptions } from 'cors';
+
+// export const corsConfig: CorsOptions = {
+//   origin: function (origin, callback) {
+//     const whitelist = [process.env.FRONTEND_URL];
+//     if (process.argv.includes('--api')) {
+//       whitelist.push(undefined);
+//     }
+//     if (whitelist.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
+
 import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
-  origin: function (origin, callback) {
+  origin: (origin, callback) => {
     const whitelist = [process.env.FRONTEND_URL];
-    if (process.argv.includes('--api')) {
-      whitelist.push(undefined);
+
+    if (process.argv[2] === '--api') {
+      whitelist.push(undefined, null); // Permitir solicitudes sin origen (Postman, etc.)
     }
-    if (whitelist.includes(origin)) {
-      callback(null, true);
+
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true); // Permitir acceso
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Error de CORS: origen no permitido')); // Bloquear acceso
     }
   },
 };
